7b525e17774ab29fd615b443fb60720d433c46a7
Cal Alstad <Cal.Alstad@elev.ga.ntig.se>
2025-09-25 14:30:17 +0200

    base of game with health being taken and a randomized level and damage

---
+++ src/Hero.java
@@ -0,0 +1,12 @@
+public class Hero {
+
+    String name;
+    int health;
+    int level;
+    int damage;
+
+    void attack(){
+        System.out.print(name + " attack!");
+    }
+
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,58 @@
+import java.util.Random;
+
 public class Main {
+
+    static Random randomGenerator = new Random();
+
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        //gives each character their stats
+        Hero myHero = new Hero();
+        myHero.name = "Athena";
+        myHero.health = 100;
+        myHero.level = (randomGenerator.nextInt(2, 10));
+        myHero.damage = (randomGenerator.nextInt(25, 100));
+
+        Hero myMonster = new Hero();
+        myMonster.name = "Ares";
+        myMonster.health = 100;
+        myMonster.level = (randomGenerator.nextInt(2, 10));
+        myMonster.damage = (randomGenerator.nextInt(25, 100));
+
+        //writes out the health and the level of each character
+        System.out.println(myHero.name + " has " + myHero.health + " health and a level of " + myHero.level);
+        System.out.println(myMonster.name + " has " + myMonster.health + " health and a level of " + myMonster.level);
+
+        //EXPLAINS HOW DAMAGE WORKS
+        System.out.println("\nThe attack damage is based on the other player's level.");
+        System.out.println("Random dmg 1-200 divided by other player's level\n");
+
+
+        // Athena attackerar Ares
+        System.out.println(myHero.name + " attacks " + myMonster.name + "!");
+        System.out.println(myHero.damage + " Basedamage");
+        myMonster.health = standardAttack(myHero.damage, myMonster.health, myMonster.level);
+        System.out.println(myMonster.name + " now has " + myMonster.health + " health left.");
+
+        System.out.println(" ");
+
+        //write out new health
+        System.out.println(myHero.name + " now has " + myHero.health + " health");
+        System.out.println(myMonster.name + " now has " + myMonster.health + " health");
     }
+
+    public static int standardAttack(int damage, int health, int level) {
+        double actualDamage = (double) damage / level;
+        int damageDone = (int) Math.ceil(actualDamage); // avrunda uppåt så att minst 1 tas
+        System.out.println("Damage dealt: " + damageDone);
+        health -= damageDone;
+        return Math.max(health, 0);
+    }
+
+
+
+
+
 }
+
+
